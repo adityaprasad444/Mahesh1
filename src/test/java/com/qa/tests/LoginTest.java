f4d70fff5a9239431e4b23c5aa227693e7c8de40
mnukala29 <mnukala29@gmail.com>
2018-06-18 08:51:54 +0530

    Made changes for jenkins

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -2,8 +2,17 @@ package com.qa.tests;
 
 import org.testng.annotations.Test;
 
+
+/**
+ * LoginTest.java - It is used to used for the login
+ * @author Nukala Mahesh
+ * @version 1.8
+ * Date - 06-18-2018	
+ */
 public class LoginTest {
 
+	
+	
 	@Test
 	public void test1(){
 		System.out.println("This is test1");
