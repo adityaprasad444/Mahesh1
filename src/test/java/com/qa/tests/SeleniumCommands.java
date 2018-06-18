21a713d3b9ce05df780e32466565ecbe380fc38e
mnukala29 <mnukala29@gmail.com>
2018-06-18 20:23:08 +0530

    Made changes to learn selenium commands

---
+++ src/test/java/com/qa/tests/SeleniumCommands.java
@@ -0,0 +1,44 @@
+package com.qa.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class SeleniumCommands {
+	
+	public WebDriver driver;
+	
+	@Test
+	public void testSeleniumCommands(){
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		
+		driver= new ChromeDriver();
+		
+		
+			
+			 
+			driver.get("https://www.facebook.com");
+			WebElement txtUserName = driver.findElement(By.id("email"));
+			 
+			Actions builder = new Actions(driver);
+			Action seriesOfActions = builder.moveToElement(txtUserName)
+			 .click()
+			 .keyDown(txtUserName, Keys.SHIFT)
+			 .sendKeys(txtUserName,"fnjdfjd")
+			 .keyUp(txtUserName, Keys.SHIFT)
+			 .doubleClick(txtUserName)
+			 .doubleClick()
+			 .build();
+			
+			 seriesOfActions.perform();
+			}
+		
+		}
+
+
